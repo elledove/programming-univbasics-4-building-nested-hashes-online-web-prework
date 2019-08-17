5e704fb3bca2809ecea125676ff73643be0ca1f6
Lauren Dove <elledoveinc@gmail.com>
2019-08-17 20:05:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,6 @@
 def base_hash
-base_hash = Hash.new {key}
+base_hash = Hash.new 
+Hash.new{}
 
  # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
