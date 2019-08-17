f6ddb3caa478172535ebec764433dc89b70789cc
Lauren Dove <elledoveinc@gmail.com>
2019-08-17 22:55:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,10 +25,9 @@ def monopoly_with_third_tier
        :reading_railroad => {},
        :pennslyvania_railroad => {},
        :b_and_o_railroad => {},
-       :shortline => {}}
+       :shortline => {}
        
      }
-  
      
    }}
    
